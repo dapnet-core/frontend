3253330655e11dd6c18b26bf18169c4743b12cbf
Valentin Geyer <geyer@ihf.rwth-aachen.de>
2022-09-30 12:17:31 +0200

    Rename swedish locale to 'sv' to match ISO

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -6,8 +6,8 @@ import it from './it.json'
 import nl from './nl.json'
 import pl from './pl.json'
 import pt from './pt.json'
-import se from './se.json'
+import sv from './sv.json'
 
 export default {
-  de, en, es, fr, it, nl, pl, pt, se
+  de, en, es, fr, it, nl, pl, pt, sv
 }
